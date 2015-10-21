cbecb4f1144d52cf835107b832782ff50d8a26a1
Max Vyaznikov <maxvyaznikov@gmail.com>
2015-10-21 10:32:24 +0300

    Use pre_init signal
    Use pre_init signal instead of class_prepared

--- django_db_prefix/models.py
+++ django_db_prefix/models.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.db.models.signals import class_prepared
+from django.db.models.signals import pre_init
 
 
 def add_db_prefix(sender, **kwargs):
@@ -18,4 +18,4 @@ def add_db_prefix(sender, **kwargs):
         sender._meta.db_table = prefix + sender._meta.db_table
 
 
-class_prepared.connect(add_db_prefix)
+pre_init.connect(add_db_prefix)
