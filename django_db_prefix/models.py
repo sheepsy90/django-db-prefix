293b305f94c2a95c57660f3dde2f5e194cb257fb
Max Vyaznikov <maxvyaznikov@gmail.com>
2015-10-21 10:47:53 +0300

    Check already applied and clear unnecessary
    Check already applied and clear unnecessary features

--- django_db_prefix/models.py
+++ django_db_prefix/models.py
@@ -4,17 +4,7 @@ from django.db.models.signals import pre_init
 
 def add_db_prefix(sender, **kwargs):
     prefix = getattr(settings, "DB_PREFIX", None)
-    if isinstance(prefix, dict):
-        app_label = sender._meta.app_label.lower()
-        sender_name = sender._meta.object_name.lower()
-        full_name = app_label + "." + sender_name
-        if full_name in prefix:
-            prefix = prefix[full_name]
-        elif app_label in prefix:
-            prefix = prefix[app_label]
-        else:
-            prefix = prefix.get(None, None)
-    if prefix:
+    if prefix and not sender._meta.db_table.startswith(prefix):
         sender._meta.db_table = prefix + sender._meta.db_table
 
 
